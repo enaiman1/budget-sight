03eb73068480c29e5a481846a0c9df4989771440
Ericcentrik <enaiman1@gmail.com>
2020-10-25 20:53:29 -0400

    more bug fixes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require('path')
-require('dotenv').config
+require('dotenv').config()
 
 const users = require("./routes/api/users");
 const plaid = require("./routes/api/plaid");
